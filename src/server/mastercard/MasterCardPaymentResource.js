29a7f14f0b1d21b86fe0b062b9d70b483ac3a1a2
Filius <marije.filius@capgemini.com>
2017-02-19 18:10:52 +0100

    circular structure to JSON still fails

--- src/server/mastercard/MasterCardPaymentResource.js
+++ src/server/mastercard/MasterCardPaymentResource.js
@@ -3,26 +3,31 @@
 
 const stolenService = require('./../../../src/server/mastercard/serviceInvokers/StolenResource');
 const paymentService = require('./../../../src/server/mastercard/serviceInvokers/PaymentResource');
+const fundingService = require('./../../../src/server/mastercard/serviceInvokers/FundingResource');
 const sanctionScreeningService = require('./../../../src/server/mastercard/serviceInvokers/SanctionScreeningResource');
 
 
 
-function performSecurityChecks(req){
+function performSecurityChecks(req,res){
   // TODO: perform check to stolenService
   // TODO: perform check to sanctionScreeningService
 
   console.log('should have perfomed some security checks.')
 }
 
-function saveTransactionInBlockChain(req){
+function saveTransactionInBlockChain(req,res){
   // TODO: koppeling met Jerre zijn BlockChainCode
   console.log('should now be saved in the blockchain');
 }
 
-function startTransaction(req){
+function startTransaction(req,res){
   paymentService(req, function(error, data){
-    // TODO: errorhandling.
-    console.log('should have performed a payment.')
+    if(data){
+      console.log('payment is performed.')
+      return data;
+    } else {
+      res.send(error);
+    }
   });
 }
 
@@ -30,11 +35,13 @@ const payment = {
   payment(req, res) {
     console.log('started with the payment process.')
 
-    performSecurityChecks(req);
-    saveTransactionInBlockChain(req);
-    startTransaction(req);
+    performSecurityChecks(req,res);
+    saveTransactionInBlockChain(req,res);
+    var data = startTransaction(req,res);
 
-    res.send('First Response');
+    console.log('sending response:');
+    console.log(data);
+    res.send(data);
   }
 };
 
