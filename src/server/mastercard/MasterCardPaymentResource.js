525279a22d8409bc8833994773b587e162e7039b
Coenraad <C26918@branches.nl.eu.abnamro.com>
2017-02-20 20:02:04 +0100

    Adds startTransaction functionality with promise

--- src/server/mastercard/MasterCardPaymentResource.js
+++ src/server/mastercard/MasterCardPaymentResource.js
@@ -1,47 +1,47 @@
 'use strict';
 
-
 const stolenService = require('./../../../src/server/mastercard/serviceInvokers/StolenResource');
 const paymentService = require('./../../../src/server/mastercard/serviceInvokers/PaymentResource');
 const fundingService = require('./../../../src/server/mastercard/serviceInvokers/FundingResource');
 const sanctionScreeningService = require('./../../../src/server/mastercard/serviceInvokers/SanctionScreeningResource');
 
-
-
-function performSecurityChecks(req,res){
+function performSecurityChecks(req, res) {
   // TODO: perform check to stolenService
   // TODO: perform check to sanctionScreeningService
 
-  console.log('should have perfomed some security checks.')
+  console.log('should have perfomed some security checks.');
 }
 
-function saveTransactionInBlockChain(req,res){
+function saveTransactionInBlockChain(req, res) {
   // TODO: koppeling met Jerre zijn BlockChainCode
   console.log('should now be saved in the blockchain');
 }
 
-function startTransaction(body){
-  return paymentService(body, function(error, data){
-    console.log('in startTransaction')
-    console.log(error);
-    console.log(data);
-    return data;
+function startTransaction(body) {
+  return new Promise((resolve, reject) => {
+    paymentService(body)
+      .then(resolve)
+      .catch(reject);
   });
 }
 
 const payment = {
   payment(req, res) {
-    console.log('started with the payment process.')
+    console.log('started with the payment process.');
 
-    performSecurityChecks(req,res);
-    saveTransactionInBlockChain(req,res);
-    var data = startTransaction(req.body);
-
-
-    console.log('sending response:');
-    console.log(data);
-    res.status(200);
-    res.json(data);
+    performSecurityChecks(req, res);
+    saveTransactionInBlockChain(req, res);
+    startTransaction(req.body)
+      .then((data) => {
+        console.log('sending response:');
+        console.log(data);
+        res.status(200);
+        res.json(data);
+      })
+      .catch((error) => {
+        res.status(500);
+        res.json(error);
+      });
   }
 };
 
